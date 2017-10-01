e541c5e2c4d333769889cf243a30bbb4ac171902
DESKTOP-EHRVAU5\dso75 <dso753@naver.com>
2017-10-02 02:02:52 +0900

    캐싱을 위한 애너테이션 메소드 사용

--- src/main/java/spittr/data/JdbcSpitterRepository.java
+++ src/main/java/spittr/data/JdbcSpitterRepository.java
@@ -1,34 +1,109 @@
 package spittr.data;
 
-import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
+import spittr.data.SpitterRepository;
 import spittr.domain.Spitter;
 
-import javax.inject.Inject;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Repository
 public class JdbcSpitterRepository implements SpitterRepository {
-    private static final String INSERT_SPITTER = "";
-    private JdbcOperations jdbcOperations;
 
-    @Inject
-    public JdbcSpitterRepository(JdbcOperations jdbcOperations) {
-        this.jdbcOperations = jdbcOperations;
+    private JdbcTemplate jdbcTemplate;
 
+    @Autowired
+    public JdbcSpitterRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 
-    public void addSpitter(Spitter spitter) {
-        jdbcOperations.update(INSERT_SPITTER,
+    public long count() {
+//        return jdbcTemplate.queryForLong("select count(id) from Spitter");
+        return 0L;
+    }
+
+    @CachePut(value = "spitterCache", key = "#result.id")
+    public Spitter save(Spitter spitter) {
+        Long id = spitter.getId();
+        if (id == null) {
+            long spitterId = insertSpitterAndReturnId(spitter);
+            return new Spitter(spitterId, spitter.getUsername(), spitter.getPassword(), spitter.getFullName(), spitter.getEmail(), spitter.isUpdateByEmail());
+        } else {
+            jdbcTemplate.update("update Spitter set username=?, password=?, fullname=?, email=?, updateByEmail=? where id=?",
+                    spitter.getUsername(),
+                    spitter.getPassword(),
+                    spitter.getFullName(),
+                    spitter.getEmail(),
+                    spitter.isUpdateByEmail(),
+                    id);
+        }
+        return spitter;
+    }
+
+    private long insertSpitterAndReturnId(Spitter spitter) {
+        SimpleJdbcInsert jdbcInsert = new SimpleJdbcInsert(jdbcTemplate).withTableName("Spitter");
+        jdbcInsert.setGeneratedKeyName("id");
+        Map<String, Object> args = new HashMap<String, Object>();
+        args.put("username", spitter.getUsername());
+        args.put("password", spitter.getPassword());
+        args.put("fullname", spitter.getFullName());
+        args.put("email", spitter.getEmail());
+        args.put("updateByEmail", spitter.isUpdateByEmail());
+        long spitterId = jdbcInsert.executeAndReturnKey(args).longValue();
+        return spitterId;
+    }
+
+    private void insertSpitter(Spitter spitter) {
+        jdbcTemplate.update(INSERT_SPITTER,
                 spitter.getUsername(),
-                spitter.getPassword());
-    }
-    @Override
-    public Spitter save(Spitter unsaved) {
-        return null;
+                spitter.getPassword(),
+                spitter.getFullName(),
+                spitter.getEmail(),
+                spitter.isUpdateByEmail());
     }
 
-    @Override
-    public Spitter findByUsername(String username) {
-        return null;
+    @Cacheable("spitterCache")
+    public Spitter findOne(long id) {
+        try {
+            return jdbcTemplate.queryForObject(
+                    SELECT_SPITTER + " where id=?", new SpitterRowMapper(), id);
+        }
+        catch (EmptyResultDataAccessException e) {
+            return null;
+        }
     }
+
+    public Spitter findByUsername(String username) {
+        return jdbcTemplate.queryForObject("select id, username, password, fullname, email, updateByEmail from Spitter where username=?", new SpitterRowMapper(), username);
+    }
+
+    public List<Spitter> findAll() {
+        return jdbcTemplate.query("select id, username, password, fullname, email, updateByEmail from Spitter order by id", new SpitterRowMapper());
+    }
+
+    private static final class SpitterRowMapper implements RowMapper<Spitter> {
+        public Spitter mapRow(ResultSet rs, int rowNum) throws SQLException {
+            long id = rs.getLong("id");
+            String username = rs.getString("username");
+            String password = rs.getString("password");
+            String fullName = rs.getString("fullname");
+            String email = rs.getString("email");
+            boolean updateByEmail = rs.getBoolean("updateByEmail");
+            return new Spitter(id, username, password, fullName, email, updateByEmail);
+        }
+    }
+
+    private static final String INSERT_SPITTER = "insert into Spitter (username, password, fullname, email, updateByEmail) values (?, ?, ?, ?, ?)";
+
+    private static final String SELECT_SPITTER = "select id, username, password, fullname, email, updateByEmail from Spitter";
 }
