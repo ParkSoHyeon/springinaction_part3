3284aaf5200cd20abf877eb4843eb8d65f8ec78c
DESKTOP-EHRVAU5\dso75 <dso753@naver.com>
2017-10-02 13:58:03 +0900

    메소드 레벨 시큐리티를 위한 표현식 사용

--- src/main/java/spittr/persistence/JpaSpitterRepository.java
+++ src/main/java/spittr/persistence/JpaSpitterRepository.java
@@ -1,9 +1,15 @@
 package spittr.persistence;
 
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PostFilter;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.access.prepost.PreFilter;
 import org.springframework.stereotype.Repository;
 import spittr.data.SpitterRepository;
 import spittr.domain.Spitter;
 
+import javax.annotation.security.RolesAllowed;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceContext;
@@ -20,19 +26,37 @@ public class JpaSpitterRepository implements SpitterRepository{
     @PersistenceContext
     private EntityManager entityManager;
 
+//    @Secured({"ROLE_SPITTER", "ROLE_ADMIN"})
+//    @RolesAllowed("ROLE_SPITTER")
+    @PreAuthorize("(hasRole('ROLE_SPITTER') and #spitter.fullName.length() <= 10) or hasRole('ROLE_PREMIUM')")
     public void addSpitter(Spitter spitter) {
 //        entityManagerFactory.createEntityManager().persist(spitter);
         entityManager.persist(spitter);
     }
+
+    @PostAuthorize("returnObject.username == principal.username")
     public Spitter getSpitterById(long id) {
 //        return entityManagerFactory.createEntityManager().find(Spitter.class, id);
         return entityManager.find(Spitter.class, id);
     }
 
-
     public void saveSpitter(Spitter spitter) {
 //        entityManagerFactory.createEntityManager().merge(spitter)
         entityManager.merge(spitter);
+    }
+
+    @PreAuthorize("hasAnyRole({'ROLE_SPITTER', 'ROLE_ADMIN'})")
+    @PostFilter("hasRole('ROLE_ADMIN') || filterObject.username == principal.username")
+    public List<Spitter> getOffensiveSpitters() {
+        return null;
+    }
+
+
+    @PreAuthorize("hasAnyRole({'ROLE_SPITTER', 'ROLE_ADMIN'})")
+//    @PreFilter("hasRole('ROLE_ADMIN') || filterObject.username == principal.username")
+    @PreFilter("hasPermission(filterObject, 'delete')")
+    public void deleteSpitters(List<Spitter> spitters) {
+
     }
 
     @Override
@@ -58,5 +82,10 @@ public class JpaSpitterRepository implements SpitterRepository{
     @Override
     public List<Spitter> findAll() {
         return null;
+    }
+
+    @Override
+    public void remove(long spitterId) {
+
     }
 }
