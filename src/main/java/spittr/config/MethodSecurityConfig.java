761fcbd61db071e460e0da2aa3df0c16501231f9
DESKTOP-EHRVAU5\dso75 <dso753@naver.com>
2017-10-02 13:55:56 +0900

    맞춤형 권한 평가자 ExpressionHandler 생성

--- src/main/java/spittr/config/MethodSecurityConfig.java
+++ src/main/java/spittr/config/MethodSecurityConfig.java
@@ -1,9 +1,12 @@
 package spittr.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
+import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
+import spittr.security.SpitterPermissionEvaluator;
 
 @Configuration
 //@EnableGlobalMethodSecurity(securedEnabled = true)    //Secured annotation 사용하기 위한 설정
@@ -13,5 +16,15 @@ public class MethodSecurityConfig extends GlobalMethodSecurityConfiguration{
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
                 .withUser("user").password("password").roles("USER");
+    }
+
+    //맞춤형 권한 평가자
+    @Override
+    protected MethodSecurityExpressionHandler createExpressionHandler() {
+        DefaultMethodSecurityExpressionHandler expressionHandler = new DefaultMethodSecurityExpressionHandler();
+
+        expressionHandler.setPermissionEvaluator(new SpitterPermissionEvaluator());
+
+        return expressionHandler;
     }
 }
