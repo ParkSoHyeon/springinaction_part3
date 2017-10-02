27843f125d3be8699a16e35abba0721357fe57ad
DESKTOP-EHRVAU5\dso75 <dso753@naver.com>
2017-10-02 14:00:22 +0900

    맞춤형 권한 평가자 구현

---
+++ src/main/java/spittr/security/GrantedAuthorityImpl.java
@@ -0,0 +1,9 @@
+package spittr.security;
+
+import org.springframework.security.core.GrantedAuthority;
+
+public class GrantedAuthorityImpl implements GrantedAuthority {
+    public GrantedAuthorityImpl(String role_admin) {
+
+    }
+}
---
+++ src/main/java/spittr/security/SpitterPermissionEvaluator.java
@@ -0,0 +1,33 @@
+package spittr.security;
+
+import org.springframework.security.access.PermissionEvaluator;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import spittr.domain.Spitter;
+
+import java.io.Serializable;
+
+public class SpitterPermissionEvaluator implements PermissionEvaluator{
+    private static final GrantedAuthority ADMIN_AUTHORITY = new GrantedAuthorityImpl("ROLE_ADMIN");
+    @Override
+    public boolean hasPermission(Authentication authentication, Object targetDomainObject, Object permission) {
+        if(targetDomainObject instanceof Spitter) {
+            Spitter spitter = (Spitter) targetDomainObject;
+            String username = spitter.getUsername();
+
+            if("delete".equals(permission)) {
+                return isAdmin(authentication) || username.equals(authentication.getName());
+            }
+        }
+        throw new UnsupportedOperationException("hasPermission not supported for object <" + targetDomainObject + "> and permission <" + permission + ">")
+    }
+
+    @Override
+    public boolean hasPermission(Authentication authentication, Serializable targetId, String targetType, Object permission) {
+        throw new UnsupportedOperationException();
+    }
+
+    private boolean isAdmin(Authentication auth) {
+        return auth.getAuthorities().contains(ADMIN_AUTHORITY);
+    }
+}
